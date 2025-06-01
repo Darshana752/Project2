9d7828cb0f85467e842f728d2276cfad5aaa1bee
Darshana752 <malithdarshana2000@gmail.com>
2025-06-01 23:59:19 +0530

    apahu haduwa

--- connection.php
+++ connection.php
@@ -1,7 +1,12 @@
+
+
+<?php
 $connection = mysqli_connect('localhost','root','','project2');
 
 if(mysqli_connect_error()){
-  die('database connection fail'.mysqli_connect_error())
+    echo "fail";
 }else{
+
   echo "success connection";
 }
+ ?>
