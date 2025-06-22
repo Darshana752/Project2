1af2fb32abbb09222d42395b9bebc5260ec337da
Darshana752 <malithdarshana2000@gmail.com>
2025-06-22 10:39:31 +0530

    loginreadbook page was updated

---
+++ loginreadbook.php
@@ -0,0 +1,30 @@
+<?php include 'connection.php'; ?>
+
+<?php 
+
+if(isset($_POST['submit'])){
+
+   $username = mysqli_real_escape_string($connection,$_POST['username']);
+   $password = mysqli_real_escape_string($connection,$_POST['password']);
+
+
+   $stmt = $connection -> prepare("SELECT * FROM details WHERE username=? AND lpassword=? LIMIT 1");
+   $stmt -> bind_param("ss", $username,$password);
+   $stmt->execute();
+   $stmt_result= $stmt->get_result();
+   if($stmt_result -> num_rows ===1){
+     echo "success";
+   }else{
+      echo "unsuccess ";
+   }
+
+
+
+
+
+}
+
+
+
+
+?>
